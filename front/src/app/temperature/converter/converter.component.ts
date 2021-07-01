5f274b5ddac480cc9aec1eb081de160b4b2b37f9
Swinkaran <swinkaran@hotmail.com>
2021-07-02 02:38:28 +1000

    Applying filter

--- front/src/app/temperature/converter/converter.component.ts
+++ front/src/app/temperature/converter/converter.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Subscription } from 'rxjs';
 import { ITemperature } from '../temperature';
 import { TemperatureService } from '../temperatureService';
@@ -9,15 +10,23 @@ import { TemperatureService } from '../temperatureService';
   styleUrls: ['./converter.component.css']
 })
 export class ConverterComponent implements OnInit {
-
+  pageTitle: string = 'Temperature converter';
   sub!: Subscription;  
   errorMessage = '';
   temperatures: ITemperature[] = [];  
-  
+  selectedFromOption: string = 'Celsius';
+  selectedToOption: string = 'Celsius';
+  temperatureInput:string='';
+  temperatureOutput:string='';
+
+  modifiedText:string='';
+
   constructor(private temperatureService: TemperatureService) { }
 
   ngOnInit(): void {
     console.log('reading the data');
+
+    //
     this.sub = this.temperatureService.gettemperatures().subscribe({
       next: temperatures => {
         this.temperatures = temperatures;
@@ -35,5 +44,24 @@ export class ConverterComponent implements OnInit {
   ngOnDestroy(): void {
     this.sub.unsubscribe();
   }
+  
+  convertMetric()
+  {
+    console.log('Converting '+ this.temperatureInput +' from ' + this.selectedFromOption + ' to ' + this.selectedToOption + ' ');
+    //call api
+    //this.customFunction(val);
+    
+  }
 
+  onTemperatureSElected(val : any)
+  {
+    //call api
+    this.customFunction(val);
+    
+  }
+
+  customFunction(val: any)
+  {
+    this.modifiedText = 'The value seleced is '+ val + '';
+  }
 }
--- front/src/app/temperature/temperature.module.ts
+++ front/src/app/temperature/temperature.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { ConverterComponent } from './converter/converter.component';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';  
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
     declarations: [
@@ -9,6 +10,7 @@ import { CommonModule } from '@angular/common';
     ],
     imports: [
         CommonModule,
+        FormsModule,
       RouterModule.forChild([
         { path: 'temperature', component: ConverterComponent },
       ]),
